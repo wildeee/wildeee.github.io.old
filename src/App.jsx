3ea3011ffbdf83400b94f1b9fae0a113c8fd372e
Wilde <gutopicciani@yahoo.com.br>
2018-02-17 02:10:15 -0200

    Ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
@@ -9,26 +8,25 @@ import Typography from 'material-ui/Typography';
 const styles = {
   root: {
     width: '100%',
+    paddingTop: '10px',
+    paddingRight: '20px'
   },
 };
 
-function App(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Toolbar>
-          <Typography variant="title" color="inherit">
-            Bem vindo!
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className={this.props.root}>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              Bem vindo!
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
-
-App.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
 
 export default withStyles(styles)(App);
