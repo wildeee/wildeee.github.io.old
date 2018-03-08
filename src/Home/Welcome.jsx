7f7d17e54bf390c247eab8fe9d7d439abb12f9a8
Wilde <gutopicciani@yahoo.com.br>
2018-03-07 22:00:23 -0300

    Anos de idade dinamicos

--- src/Home/Welcome.jsx
+++ src/Home/Welcome.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import Typography from 'material-ui/Typography';
+import moment from 'moment';
 
 class Welcome extends Component {
   render() {
+    const yearsOld = moment().diff('1994-10-18', 'years');
     return (
       <div>
         <Typography variant="title" color="inherit" align="center" gutterBottom>
           Olá!
         </Typography>
         <Typography variant="body2" align="justify">
-          Meu nome é Wilde, tenho 23 anos, atuo na área de desenvolvimento de sistemas de software, e sou um eterno estudante de tecnologia.
+          Meu nome é Wilde, tenho {yearsOld} anos, atuo na área de desenvolvimento de sistemas de software, e sou um eterno estudante de tecnologia.
           Criei esta página como uma alternativa ao currículo clássico, e também para conter outros conteúdos extra, então sinta-se livre para navegar.
         </Typography>
       </div>
